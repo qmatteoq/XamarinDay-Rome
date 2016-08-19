1e414b70324a94f38526d2599ae8964238e82b16
Matteo Pagani <matteo.pagani@microsoft.com>
2016-08-19 18:34:35 +0200

    Update of README

--- TrackSeries/TrackSeries/ViewModels/MainPageViewModel.cs
+++ TrackSeries/TrackSeries/ViewModels/MainPageViewModel.cs
@@ -20,7 +20,7 @@ namespace TrackSeries.ViewModels
             get { return _topSeries; }
             set { SetProperty(ref _topSeries, value); }
         } 
-
+          
         public MainPageViewModel(TsApiService apiService)
         {
             _apiService = apiService;
