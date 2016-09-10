47ea3d6e8bc9228e3abbd9f74c6633f2a0d2c091
Matteo Pagani <matteo.pagani@microsoft.com>
2016-09-10 15:28:26 +0200

    Removed fixed URL of the Azure Mobile App

--- TrackSeries/TrackSeries/Models/Constants.cs
+++ TrackSeries/TrackSeries/Models/Constants.cs
@@ -2,7 +2,6 @@
 {
     public static class Constants
     {
-        //public static string MobileAppUrl = "https://xamarinday-csharp.azurewebsites.net/";
-        public static string MobileAppUrl = "https://xamarinday-nodejs.azurewebsites.net";
+        public static string MobileAppUrl = "<replace with the URL of your Azure Mobile app>";
     }
 }
